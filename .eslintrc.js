d992536c9dcb7812cb2522e938f7954cef19180a
Anton Goroshnikov <goroshnikovanton@gmail.com>
2021-08-12 21:22:08 +0300

    tailwind + vue3 inited

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'never',
+    }],
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-red-700">
+    SK MGS
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
