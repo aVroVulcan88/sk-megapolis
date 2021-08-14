161f74b8e3b4a1953092d25db3bf76621d4c9a7f
Anton Goroshnikov <goroshnikovanton@gmail.com>
2021-08-14 11:35:04 +0300

    reinit vue3 + vite + tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="text-red-700">
-    SK MGS
-  </div>
+  <router-link
+    :to="{ name: 'home' }"
+    class="text-red-700">
+    HOME
+  </router-link>
+  <router-link :to="{ name: 'about' }">
+    ABOUT
+  </router-link>
+  <router-view v-slot="{ Component }">
+    <transition
+      name="fade"
+      mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
-<script setup>
-
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup></script>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="home">
+    HOME
+  </div>
+</template>
